46d1395b22cd54a76aeb19d324c4cd8ae85f30c6
Miguel Lopez Ruiz <miguel.lr96@gmail.com>
2023-04-15 14:33:00 +0200

    Commit on '2023-04-15 14:33:00'

--- .config/zsh/config.zsh
+++ .config/zsh/config.zsh
@@ -240,8 +240,9 @@ function workspace-path() {
 
 # SSH
 function ssh() {
-    printf "\e[?2004l"
-    command ssh "$@"
+
+  printf "\e[?2004l"
+  command ssh "$@"
 }
 
 function ssh-legacy() {
@@ -252,15 +253,24 @@ function ssh-legacy() {
 
 function ssh_menu() {
     local entries entry
+    local profile hook
 
     entries=$(cat $HOME/.ssh/config | grep -i "^Host" | grep -v "*" | sed 's/ \+/ /g' | cut -d" " -f2-100000 | sort)
     entry=$(printf "$entries" | fzf)
     [ -z "$entry" ] && return 
 
+    # Password
+    if [ -e $HOME/.ssh/credentials ]; then
+      profile=$(cat $HOME/.ssh/credentials | cut -d'|' -f 1 | fzf)
+      if [ -n "$profile" -a "${profile}" != "NONE" ];  then
+        hook="sshpass -p'$(cat $HOME/.ssh/credentials | grep ^$profile | cut -d'|' -f 2)'"
+      fi
+    fi
+
     echo "[+] Connect to '$(echo $entry | cut -d" " -f1)'"
 
     printf "\e[?2004l"
-    eval "TERM=xterm-256color ssh $(echo $entry | cut -d" " -f1)"
+    eval "TERM=xterm-256color $hook ssh $(echo $entry | cut -d" " -f1)"
 }
 
 function docker-upload-image () {
