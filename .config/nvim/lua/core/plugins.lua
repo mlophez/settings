7c93f16085f7a5cc02e5b2c3e0458ba2d3cbb9c2
Miguel López Ruiz <miguel.lr96@gmail.com>
2024-01-21 14:18:46 +0100

    Commit on '2024-01-21 14:18:46'

--- .config/nvim/lua/core/plugins.lua
+++ .config/nvim/lua/core/plugins.lua
@@ -250,6 +250,29 @@ lazy.setup({
 		opts = require("plugins.toggleterm"),
 	},
 
+	{
+		"nvim-neorg/neorg",
+		build = ":Neorg sync-parsers",
+		cmd = "Neorg",
+		ft = "norg",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("neorg").setup({
+				load = {
+					["core.defaults"] = {}, -- Loads default behaviour
+					["core.concealer"] = {}, -- Adds pretty icons to your documents
+					["core.dirman"] = { -- Manages Neorg workspaces
+						config = {
+							workspaces = {
+								notes = "~/Documents/Notes",
+							},
+						},
+					},
+				},
+			})
+		end,
+	},
+
 	--{
 	--	"akinsho/flutter-tools.nvim",
 	--	lazy = false,
