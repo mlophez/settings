c7a267bed5cf4785efcb2daf1d2468f253355771
Miguel López Ruiz <miguel.lr96@gmail.com>
2024-03-05 17:35:37 +0100

    Commit on '2024-03-05 17:35:37'

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -11,8 +11,8 @@ return {
 	},
 	keys = {
 		{ "<C-p>", ":Telescope find_files<cr>", silent = true },
-		--{ "<leader>f", ":Telescope find_files<cr>", silent = true },
-		{ "<leader>f", ":Telescope live_grep<cr>", silent = true },
+		{ "<leader>f", ":Telescope find_files<cr>", silent = true },
+		{ "<leader>s", ":Telescope live_grep<cr>", silent = true },
 	},
 	opts = {
 		defaults = {
