803c84de208061147b4d50f83426f3ab23704d04
Miguel López Ruiz <miguel.lr96@gmail.com>
2024-04-16 20:27:06 +0200

    Commit on '2024-04-16 20:27:06'

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -9,9 +9,9 @@ return {
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
   keys = {
-    -- { "m",         ":Neotree toggle filesystem reveal float<cr>", silent = true },
-    { "m",         ":Neotree toggle filesystem reveal right<cr>", silent = true },
-    { "<leader>e", ":Neotree toggle filesystem reveal right<cr>", silent = true },
+    { "m",         ":Neotree toggle filesystem reveal float<cr>", silent = true },
+    { "<leader>e", ":Neotree toggle filesystem reveal float<cr>", silent = true },
+    --{ "m",         ":Neotree toggle filesystem reveal right<cr>", silent = true },
   },
   config = function()
     require("neo-tree").setup({
