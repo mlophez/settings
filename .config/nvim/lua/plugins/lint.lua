f45d1947e7368c3ebd0a8987e67b62350c056b77
Miguel López Ruiz <miguel.lr96@gmail.com>
2024-05-01 12:53:08 +0200

    Commit on '2024-05-01 12:53:08'

--- .config/nvim/lua/plugins/lint.lua
+++ .config/nvim/lua/plugins/lint.lua
@@ -1,26 +1,27 @@
 return {
-	"mfussenegger/nvim-lint",
-	event = { "BufReadPost", "BufNewFile" },
-	cmd = { "Lint" },
-	config = function()
-		local lint = require("lint")
+  "mfussenegger/nvim-lint",
+  event = { "BufReadPost", "BufNewFile" },
+  cmd = { "Lint" },
+  config = function()
+    local lint = require("lint")
 
-		lint.linters_by_ft = {
-			--python = { "pylint" },
-			--python = { "pylint", "mypy" },
-			-- terraform = { "tflint", "trivy" },
-		}
+    lint.linters_by_ft = {
+      go = { "golangcilint" },
+      --python = { "pylint" },
+      --python = { "pylint", "mypy" },
+      -- terraform = { "tflint", "trivy" },
+    }
 
-		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-			group = lint_augroup,
-			callback = function()
-				lint.try_lint()
-			end,
-		})
+    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+      group = lint_augroup,
+      callback = function()
+        lint.try_lint()
+      end,
+    })
 
-		vim.api.nvim_create_user_command("Lint", function()
-			lint.try_lint()
-		end, {})
-	end,
+    vim.api.nvim_create_user_command("Lint", function()
+      lint.try_lint()
+    end, {})
+  end,
 }
