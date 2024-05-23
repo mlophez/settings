35c5a69e07f7b497ed3308357e6e1f1a3e2facaf
Miguel López Ruiz <miguel.lr96@gmail.com>
2024-05-23 18:48:32 +0200

    Commit on '2024-05-23 18:48:32'

---
+++ .config/nvim/lua/plugins/rest.lua
@@ -0,0 +1,34 @@
+return {
+  "rest-nvim/rest.nvim",
+
+  ft = "http",
+
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    {
+      {
+        "vhyrro/luarocks.nvim",
+        priority = 1000,
+        config = true,
+        opts = {
+          rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" }
+        }
+      },
+    }
+  },
+
+  opts = {
+    skip_ssl_verification = true,
+  },
+
+  config = function(_, opts)
+    require("rest-nvim").setup(opts)
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "http",
+      callback = function()
+        local kopts = { noremap = true, silent = true, buffer = tonumber(vim.fn.expand("<abuf>", 10)) }
+        vim.keymap.set("n", "<cr>", "<cmd>Rest run<cr>", kopts)
+      end,
+    })
+  end,
+}
