54a7e14d5fefb2566233018d66888c18d74dd95e
Miguel López Ruiz <miguel.lr96@gmail.com>
2024-12-09 08:16:51 +0100

    Commit on '2024-12-09 08:16:51'

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,16 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    bigfile = { enabled = true },
+    quickfile = { enabled = true },
+    rename = { enabled = true },
+    lazygit = { enabled = true }
+  },
+  keys = {
+    { "<leader>cr", function() Snacks.rename.rename_file() end, desc = "Rename File" },
+    { "<leader>gg", function() Snacks.lazygit() end,            desc = "Lazygit" },
+    { "<leader>gl", function() Snacks.lazygit.log() end,        desc = "Lazygit Log (cwd)" },
+  }
+}
