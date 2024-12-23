d8f6206ca668c5f78f4a68079807c88c0b97691c
Miguel López Ruiz <miguel.lr96@gmail.com>
2024-12-23 17:11:48 +0100

    Commit on '2024-12-23 17:11:48'

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,4 +1,8 @@
 return {
   "github/copilot.vim",
   lazy = false,
+  config = function()
+    vim.g.copilot_no_tab_map = true
+    vim.api.nvim_set_keymap("i", "<A-Tab>", 'copilot#Accept("<CR>")', { expr = true, silent = true, noremap = true })
+  end
 }
