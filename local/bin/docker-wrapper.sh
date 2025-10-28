dd993db93875e65f4c89fb1e9f9d57d19a8218cd
Miguel López Ruiz <miguel.lr96@gmail.com>
2025-10-28 13:37:19 +0100

    Commit on '2025-10-28 13:37:19'

--- local/bin/docker-wrapper.sh
+++ local/bin/docker-wrapper.sh
@@ -2,14 +2,23 @@
 
 # A simple wrapper to support container in macos and podman in linux
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    # macOS - use Docker
+container_wrapper() {
+    # macOS - use container
+    container system status &>/dev/null || container system start
     # if is docker login change by container registry login
+    [[ "$1" == "container" ]] && shift
+    [[ "$1" == "containers" ]] && shift
+
     [[ "$1" == "login" ]] && shift && set -- "registry" "login" "$@"
     [[ "$1" == "push" ]]  && shift && set -- "image" "push" "$@"
     [[ "$1" == "pull" ]]  && shift && set -- "image" "pull" "$@"
+    [[ "$1" == "ps" ]] && shift && set -- "ls" "$@"
+
     container "$@"
+}
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    container_wrapper "$@"
 else
-    # Linux - use Podman
     podman "$@"
 fi
