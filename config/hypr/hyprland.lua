b9d5f4f8d28319badb69c7c2ac7ae7fbda98dd83
Miguel López Ruiz <miguel.lr96@gmail.com>
2026-05-13 17:34:05 +0200

    Add env to hyprland 2

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -167,7 +167,7 @@ hl.bind("SUPER + RETURN", run_terminal, { description = "Terminal" })
 hl.bind("SUPER + Z", hl.dsp.exec_cmd("deskcmd lock"), { description = "Lock" })
 hl.bind("SUPER + I", hl.dsp.exec_cmd("makoctl dismiss"), { description = "Dismiss notification" })
 
-hl.bind("SUPER + SHIFT + RETURN", run_terminal, { description = "Terminal" })
+hl.bind("SUPER + SHIFT + RETURN", hl.dsp.exec_cmd("uwsm-app -- ptyxis --new-window"), { description = "Backup Terminal" })
 hl.bind("F1", hl.dsp.exec_cmd([[sh -c 'echo "exec ok at $(date)"' > /tmp/hyprtest.log]]),
   { description = "Hypr test ping" })
 hl.bind("SUPER + SHIFT + E", hl.dsp.exec_cmd("deskcmd menu exit"), { description = "Exit menu" })
