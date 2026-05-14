04c650c158dbcbb02d614be117e8c9a26402984f
Miguel López Ruiz <miguel.lr96@gmail.com>
2026-05-14 11:49:00 +0200

    Migrate a fish 3

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,21 +1,22 @@
 if not status is-interactive
     return
 end
-
-set -g fish_greeting
-
 umask 022
 
-# Cursor shape: blinking bar antes de cada prompt
-function __set_cursor --on-event fish_prompt
-    printf '\e[5 q'
-end
-
+set -g fish_greeting
 # Fix nixGL
 set -e LD_LIBRARY_PATH
 set -e LIBGL_DRIVERS_PATH
 set -e LIBVA_DRIVERS_PATH
 set -e __EGL_VENDOR_LIBRARY_FILENAMES
+
+# THEMES
+fish_config theme choose catppuccin-mocha --color-theme=dark
+
+# Cursor shape: blinking bar antes de cada prompt
+function __set_cursor --on-event fish_prompt
+    printf '\e[5 q'
+end
 
 # Starship prompt
 if type -q starship
@@ -27,5 +28,3 @@ if type -q kubectl
     kubectl completion fish | source
 end
 
-# THEMES
-fish_config theme choose catppuccin-mocha --color-theme=dark
