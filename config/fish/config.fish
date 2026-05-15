2fc6fec1d61aa5a4e7362a1920c68a77bd77fc53
Miguel López Ruiz <miguel.lr96@gmail.com>
2026-05-15 16:26:35 +0200

    Shell speed 2

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -11,7 +11,7 @@ set -e LIBVA_DRIVERS_PATH
 set -e __EGL_VENDOR_LIBRARY_FILENAMES
 
 # THEMES
-#fish_config theme choose catppuccin-mocha --color-theme=dark
+fish_config theme choose catppuccin-mocha --color-theme=dark
 
 # Cursor shape: blinking bar antes de cada prompt
 function __set_cursor --on-event fish_prompt
