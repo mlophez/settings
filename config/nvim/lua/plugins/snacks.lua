601a3e77af7c3e2236142e1c0175e4e6ecfc7cab
Miguel López Ruiz <miguel.lr96@gmail.com>
2026-06-30 13:57:09 +0200

    Changes in dotfiles 1

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -6,14 +6,16 @@ return {
     bigfile = { enabled = true },
     quickfile = { enabled = true },
     lazygit = { enabled = true },
+    picker = { enabled = true },
     input = { enabled = true },
     -- scroll = { enabled = true }
     -- rename = { enabled = false },
     -- bufdelete = { enabled = true },
   },
   keys = {
-    { "<leader>cr", function() Snacks.rename.rename_file() end, desc = "Rename File" },
-    { "<leader>g",  function() Snacks.lazygit() end,            desc = "Lazygit" },
+    { "<leader>cr", function() Snacks.rename.rename_file() end,  desc = "Rename File" },
+    { "<leader>g",  function() Snacks.lazygit() end,             desc = "Lazygit" },
+    { "<leader>gl", function() Snacks.picker.git_log_file() end, desc = "Git file history" },
     --{ "<leader>gl", function() Snacks.lazygit.log() end,        desc = "Lazygit Log (cwd)" },
   }
 }
